54b3cf4ccaed10cdbe7354ddb5014e32cc6c78e0
Taro Minowa <higepon@gmail.com>
2016-01-01 21:45:32 -0800

    Window!

--- src/pebble-test-app.c
+++ src/pebble-test-app.c
@@ -1,5 +1,30 @@
 #include <pebble.h>
 
-int main(void) {
+Window *window;
+
+void init() {
+  // Create the Window
+  window = window_create();
+
+  // Push to the stack, animated
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  // Destroy the Window
+  window_destroy(window);
+}
+
+int main() {
+  // Initialize the app
+  init();
+
+  // Wait for app events
   app_event_loop();
+
+  // Deinitialize the app
+  deinit();
+
+  // App finished without error
+  return 0;
 }
