ee00696f446ebf1f563ee39c78601bcc5fa07981
Taro Minowa <higepon@gmail.com>
2016-01-02 11:34:58 -0800

    logging example.

--- src/pebble-test-app.c
+++ src/pebble-test-app.c
@@ -17,8 +17,27 @@ void deinit() {
   window_destroy(window);
 }
 
+void try_log() {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Data logging Start");
+
+  DataLoggingSessionRef logging_session = data_logging_create(0x1234, DATA_LOGGING_UINT, 4,
+                                                              false);
+  // Fake creating some data and logging it to the session.
+  uint32_t data[] = { 1, 2, 3};
+  data_logging_log(logging_session, &data, 3);
+
+  // Fake creating more data and logging that as well.
+  uint32_t data2[] = { 1, 2 };
+  data_logging_log(logging_session, &data2, 2);
+
+  // When we don't need to log anything else, we can close off the session.
+  data_logging_finish(logging_session);
+}
+
 int main() {
+  try_log();
   init();
+  try_log();
   app_event_loop();
   deinit();
   return 0;
